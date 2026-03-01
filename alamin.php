78758cf9c8288c2a905de73afc532a5eabe455e5
Alamin <mohammadalamin23522210@gmail.com>
2026-03-01 11:30:25 +0600

    alamin.php

---
+++ alamin.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+  <head></head>
+  <body>
+    <h1>Alamin</h1>
+    <p>Alamin is a software engineer with a passion for coding and problem-solving. He has experience in various programming languages and enjoys working on innovative projects.</p>
+    <script>
+      console.log("Alamin is a talented software engineer who loves to code and solve problems.");
+      alert("Welcome to Alamin's profile!");
+      var name="Alamin";
+      var age=30;
+      var is_human=true;
+      var x;
+      var y=null;
+      
+      hello="there we did not dadta type";
+      console.log(typeof name);
+      console.log(typeof age);   
+      console.log(typeof is_human);
+      console.log(typeof + x);
+      console.log(typeof + y);
+      var num1=10;
+      var num2=20;
+      console.log("The sum of the numbers is: " , num1 + num2); // Addition
+      console.log("The difference of the numbers is: " , num1 - num2); // Subtraction
+      console.log("The product of the numbers is: " , num1 * num2); // Multiplication
+      console.log("The quotient of the numbers is: " , num1 / num2); // Division
+      console.log("The remainder of the numbers is: " , num1 % num2); // Modulus
+      
+    </script>
+  </body>
+
+
+</html>
