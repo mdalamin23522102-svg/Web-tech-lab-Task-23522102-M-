ee0bb71c4e887e16a034b73784d759481f6c8862
md alamin <mohammadalamin2352210@gmail.com>
2026-03-10 11:32:30 +0600

    Calculator Task

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+
+function clearDisplay(){
+    display.value = "";
+}
